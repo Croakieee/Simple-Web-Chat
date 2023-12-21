77bda5d1eb1e71347a4e04ace52f0dc6e320ac28
croakie_ <71844595+Croakieee@users.noreply.github.com>
2023-12-21 21:45:01 +0400

    Create Example Code (Django Model).py

---
+++ Example Code (Django Model).py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    # Additional fields for media files, e.g., FileField for attachments
